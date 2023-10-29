2cb6b5910972b6c5dd14a6ab724df09e75e33a09
Sovereign Shahid <sovereignshahid@gmail.com>
2023-10-28 19:40:18 -0600

    styled stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,27 @@
-<h1>Sovereign's Portfolio</h1>
-<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">heres a link</a>
+<div class="main">
+  <h1>Sovereign's Portfolio</h1>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Here is a link</a>
+</div>
+
+<style>
+  .main {
+    color: #cdd6f4;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
+  h1 {
+    font-size: 4em;
+  }
+  a {
+    text-decoration: none;
+    font-size: 2em;
+    color: #74c7ec;
+    align-self: center;
+  }
+</style>
